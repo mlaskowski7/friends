95501999d95b9f3e0b52ca2844f537ae01e7bf80
mlaskowski7 <mtlaskowski7@gmail.com>
2023-12-27 14:10:27 +0100

    profile pics functionality through admin panel

--- friendsProject/users/admin.py
+++ friendsProject/users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
-# superuser: user-admin password-admin123
+# superuser: user-admin password-superuser123
+admin.site.register(Profile)
--- friendsProject/users/models.py
+++ friendsProject/users/models.py
@@ -9,4 +9,4 @@ class Profile(models.Model):
     photo = models.ImageField(upload_to='users/%Y/%m/%d',blank=True)
 
     def __str__(self):
-        return self.user
+        return self.user.username
