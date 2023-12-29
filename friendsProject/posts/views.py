123c2d4e027089525f36e46d856e90d429134e73
Mateusz Laskowski <mlaskowski7>
2023-12-29 13:54:19 +0100

    comment func, finished project

--- friendsProject/posts/views.py
+++ friendsProject/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import PostCreateForm
+from .forms import PostCreateForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from .models import Post
 from django.shortcuts import get_object_or_404, redirect
@@ -20,9 +20,23 @@ def postCreateView(request):
 
 @login_required
 def feedView(request):
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            post_id = request.POST.get('post_id')
+            created_by = request.POST.get('posted_by')
+            post = get_object_or_404(Post, id=post_id)
+            new_comment.posted_by=created_by
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     posts = Post.objects.all()
     logged_user = request.user
-    return render(request,'posts/feed.html',{'posts':posts, 'logged_user':logged_user})
+    return render(request,'posts/feed.html',{'posts':posts, 'logged_user':logged_user, 'comment_form':comment_form})
 
 @login_required
 def like_postView(request):
